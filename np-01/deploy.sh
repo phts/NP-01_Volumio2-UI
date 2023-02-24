b5bb6e18c1c30f23b41ac1c06586a58b878b6bfe
Phil Tsaryk <phil@tsaryk.com>
2023-02-24 22:19:37 +0100

    feat: hide "Tweet" button

--- np-01/deploy.sh
+++ np-01/deploy.sh
@@ -4,3 +4,6 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 scp ${SCRIPT_DIR}/../src/app/i18n/locale-ru.json \
   volumio:/volumio/http/www4/app/i18n/
+
+# scp ${SCRIPT_DIR}/../src/app/themes/volumio/assets/variants/volumio/volumio-settings.json \
+#   volumio:/volumio/http/www4/app/themes/volumio/assets/variants/volumio/
